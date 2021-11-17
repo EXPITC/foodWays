7108329eda027a91dba93d1e644b0bafaf7440d9
EXPITC <indratc18@gmail.com>
2021-11-17 07:22:01 +0700

    first commit on landing page

--- components/login/Login.style.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Wrapper = styled.div``;
-
-export const Content = styled.div``;
-
-export const input = styled.input``;
-
-export const button = styled.button``;
